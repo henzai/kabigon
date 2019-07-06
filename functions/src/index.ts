ca59ac4362d0c7dce6e66aa46cd2fd033e732e7e
ryosuke watanabe <ry0chord@gmail.com>
2019-07-07 03:55:43 +0900

    Add addMessage

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,14 +1,34 @@
 import * as functions from "firebase-functions";
-// import * as admin from 'firebase-admin';
+import * as admin from "firebase-admin";
+admin.initializeApp();
 
-// admin.initializeApp();
-
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
 //
-export const helloWorld = functions
-  .region("asia-northeast1")
-  .https.onRequest((request, response): void => {
-    response.send("Hello from Firebase!");
-  });
+export const createUser = functions
+.region("asia-northeast1")
+.firestore
+.document("sentences/{sentenceId}")
+.onCreate((snap, context):void => {
+
+  const db = admin.firestore();
+
+  const text: string = snap.get("originalText")
+
+
+  db.collection("test").add({
+    name: text,
+  })
+  
+});
+
+//
+export const addMessage = functions
+.region("asia-northeast1")
+.https.onRequest(async (req, res) :Promise<void> => {
+  // Grab the text parameter.
+  const original = req.query.text;
+  // Push the new message into the Realtime Database using the Firebase Admin SDK.
+  const snapshot = await admin.firestore().collection('/sentences').add({originalText: original});
+  // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
+  res.status(200);
+});
 
